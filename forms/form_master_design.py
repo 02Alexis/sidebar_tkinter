c49546a36995d1700f16eaad707f8e481e601ed1
02Alexis <m_chael1012@hotmail.com>
2025-02-16 10:00:37 -0500

    creación de paneles

--- forms/form_master_design.py
+++ forms/form_master_design.py
@@ -12,6 +12,7 @@ class FormMasterDesign(tk.Tk):
         self.profile = util_image.read_image("./img/perfil.jpg", (100, 100))
         self.icon = util_image.read_image("./img/profile.jpg", (32, 32)) 
         self.config_window()
+        self.panels()
 
     def config_window(self):
         # configuración inicial de la ventana
@@ -20,3 +21,18 @@ class FormMasterDesign(tk.Tk):
         w, h = 1024, 600
         self.geometry("%dx%d+0+0" % (w, h))
         util_window.center_window(self, w, h)
+
+    def panels(self):
+        # creación paneles barra superiror, menu lateral y cuerpo principal
+        self.superior_bar = tk.Frame(
+            self, bg=COLOR_UPPER_BAR, height=50
+        )
+        self.superior_bar.pack(side=tk.TOP, fill='both')
+
+        self.menu_lateral = tk.Frame(self, bg=COLOR_LATERAL_BAR, width=150)
+        self.menu_lateral.pack(side=tk.LEFT, fill='both', expand=False)
+
+        self.principal_body = tk.Frame(
+            self, bg=COLOR_MAIN_BOSY
+        )
+        self.principal_body.pack(side=tk.RIGHT, fill='both', expand=True)
